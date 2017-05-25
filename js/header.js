9e5f70361336ee9c664fb4b9542ff36446eacb61
Eric Margules <eric.i.margules@gmail.com>
2017-05-24 22:39:16 -0500

    fix header style for non index pages

---
+++ js/header.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    
+    $(window).scroll(function() {
+        if ($(window).scrollTop() > 0) {
+            $('.top_nav').removeClass('home');
+            $('.top_nav').addClass('little');
+            $("li > a").addClass('small_link');
+            $(".top_nav > nav > li > a").removeClass('white');
+            $(".top_nav > nav > li > a").addClass('black');
+        } else {
+            $('.top_nav').addClass('home');
+            $('.top_nav').removeClass('little');
+            $("li > a").removeClass('small_link');
+        }
+    });
+
+    $(window).scroll(function() {
+        if($(window).scrollTop() > 0) {
+            var top = Math.round(($(window).scrollTop() / 6));
+            $('#hero')[0].style.opacity = (1 -(top * .01));
+        }
+    });
+});
